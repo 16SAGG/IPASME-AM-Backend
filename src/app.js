d56a290982ba5b41bb288e74a91deadb8ba4668a
16SAGG <sagg160800@gmail.com>
2024-04-19 10:09:15 -0400

    appointmentRoutes_added_to_app

--- src/app.js
+++ src/app.js
@@ -3,6 +3,7 @@ import express from 'express'
 import userRoutes from './routes/users.routes.js'
 import medicalHistoryRoutes from './routes/medicalHistory.routes.js'
 import patientRoutes from './routes/patient.routes.js'
+import appointmentRoutes from './routes/appointment.routes.js'
 
 import { notFound } from './controllers/404.controllers.js'
 
@@ -13,6 +14,7 @@ app.use(express.json())
 app.use('/api', userRoutes)
 app.use('/api', patientRoutes)
 app.use('/api', medicalHistoryRoutes)
+app.use('/api', appointmentRoutes)
 
 app.use(notFound)
 
--- src/routes/appointment.routes.js
+++ src/routes/appointment.routes.js
@@ -1,12 +1,13 @@
 import { Router } from "express";
+import { createAppointment, deleteAppointment, getAppointment, getAppointments, updateAppointment } from "../controllers/appointment.controllers";
 
 const router = Router()
 
-router.get('/appointments', getUsers)
-router.get('/appointments/:id', getUser)
-router.post('/appointments', createUser)
-router.patch('/appointments/:id', updateUser )
-router.delete('/appointments/:id', deleteUser)
+router.get('/appointments', getAppointments)
+router.get('/appointments/:id', getAppointment)
+router.post('/appointments', createAppointment)
+router.patch('/appointments/:id', updateAppointment )
+router.delete('/appointments/:id', deleteAppointment)
 
 
 export default router
