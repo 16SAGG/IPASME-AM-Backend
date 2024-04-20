58dbc5f3e9f6e92fa972fa8079609002dea4981d
16SAGG <sagg160800@gmail.com>
2024-04-20 18:46:25 -0400

    userRoutes_reordered

--- src/routes/users.routes.js
+++ src/routes/users.routes.js
@@ -4,9 +4,9 @@ import {isAdmin, verifyToken} from "../middlewares/index.js"
 
 const router = Router()
 
-router.get('/users', [verifyToken, isAdmin], getUsers) 
-router.get('/users/:id', [verifyToken, isAdmin], getUser)
+router.get('/users', [verifyToken, isAdmin], getUsers)
 router.get('/users/doctors', verifyToken, getDoctors)
+router.get('/users/:id', [verifyToken, isAdmin], getUser)
 router.patch('/users/:id', [verifyToken, isAdmin], updateUser )
 router.delete('/users/:id', [verifyToken, isAdmin], deleteUser)
 
