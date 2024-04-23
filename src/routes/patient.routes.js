1592c98aa0d796c4cd5548f5e3b80819cd87a816
16SAGG <sagg160800@gmail.com>
2024-04-23 17:44:49 -0400

    getPatients_get_all_data

--- src/routes/patient.routes.js
+++ src/routes/patient.routes.js
@@ -1,11 +1,11 @@
 import { Router } from "express";
 import { createPatient, deletePatient, getPatientsSeenBySpecialtyOnADate, getPatient, getPatients, updatePatient } from "../controllers/patient.controllers.js";
-import {verifyToken, isReceptionist, isAdmin} from "../middlewares/index.js"
+import {verifyToken, isReceptionist} from "../middlewares/index.js"
 
 const router = Router()
 
 router.get('/patients', [verifyToken], getPatients)
-router.get('/patients/specialty/:id_specialty/:month/:year', getPatientsSeenBySpecialtyOnADate)
+router.get('/patients/specialty/:id_specialty/:month/:year', [verifyToken], getPatientsSeenBySpecialtyOnADate)
 router.get('/patients/:id', [verifyToken], getPatient)
 router.post('/patients', [verifyToken, isReceptionist], createPatient)
 router.patch('/patients/:id', [verifyToken, isReceptionist], updatePatient )
