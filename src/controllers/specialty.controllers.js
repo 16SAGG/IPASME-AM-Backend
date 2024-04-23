86bcbb531f00579ccbc9b26b1e15f3f818805dba
16SAGG <sagg160800@gmail.com>
2024-04-23 18:16:35 -0400

    get_specialty_added

--- src/controllers/specialty.controllers.js
+++ src/controllers/specialty.controllers.js
@@ -12,3 +12,21 @@ export const getSpecialties = async (req, res) =>{
         })
     }
 }
+
+export const getSpecialty = async (req, res) =>{
+    try{
+        const {id} = req.params
+        const [rows] = await pool.query('SELECT * FROM specialty WHERE id = ?', [id])
+
+        if (rows.length <= 0) return res.status(404).json({
+            message: 'Specialty Not Found.'
+        })
+
+        res.json(rows[0])
+    }
+    catch {
+        return res.status(500).json({
+            message : 'Something Goes Wrong'
+        }) 
+    }
+}
--- src/routes/specialty.routes.js
+++ src/routes/specialty.routes.js
@@ -1,9 +1,10 @@
 import { Router } from "express";
 import {verifyToken} from "../middlewares/index.js"
-import { getSpecialties } from "../controllers/specialty.controllers.js";
+import { getSpecialties, getSpecialty } from "../controllers/specialty.controllers.js";
 
 const router = Router()
 
 router.get('/specialty', [verifyToken], getSpecialties)
+router.get('/specialty/:id', [verifyToken], getSpecialty)
 
 export default router
