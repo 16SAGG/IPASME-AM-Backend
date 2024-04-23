6422e36f1ace72ea9ecec258da9dfd5b65dde919
16SAGG <sagg160800@gmail.com>
2024-04-23 11:45:50 -0400

    app_use_new_routes

--- src/app.js
+++ src/app.js
@@ -6,6 +6,13 @@ import medicalHistoryRoutes from './routes/medicalHistory.routes.js'
 import patientRoutes from './routes/patient.routes.js'
 import appointmentRoutes from './routes/appointment.routes.js'
 import authRoutes from './routes/auth.routes.js'
+import appointmentTypeRoutes from "./routes/appointmentType.routes.js";
+import genderRoutes from './routes/gender.routes.js'
+import specialtyRoutes from './routes/specialty.routes.js'
+import turnRoutes from './routes/turn.routes.js'
+import userTypeRoutes from './routes/userType.routes.js'
+
+
 
 import { notFound } from './controllers/404.controllers.js'
 
@@ -20,6 +27,11 @@ app.use('/api', userRoutes)
 app.use('/api', patientRoutes)
 app.use('/api', medicalHistoryRoutes)
 app.use('/api', appointmentRoutes)
+app.use('/api', genderRoutes)
+app.use('/api', appointmentTypeRoutes)
+app.use('/api', specialtyRoutes)
+app.use('/api', turnRoutes)
+app.use('/api', userTypeRoutes)
 app.use('/api', authRoutes)
 
 app.use(notFound)
