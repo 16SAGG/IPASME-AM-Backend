27e6705959d77715d9a9cc0f16e2e54a44936674
16SAGG <sagg160800@gmail.com>
2024-04-25 15:12:42 -0400

    domain-fixed-added-to-cors

--- src/app.js
+++ src/app.js
@@ -20,7 +20,7 @@ const app = express()
 
 app.use(express.json())
 app.use(cors({
-    origin: ['http://localhost:3000', 'https://ipasme.vercel.app/']
+    origin: ['http://localhost:3000', 'https://ipasme.vercel.app']
 }))
 
 app.use('/api', userRoutes)
