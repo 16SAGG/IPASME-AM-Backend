069146ce926e608fd9cce9c3f9ff3e9e9c67b1b5
16SAGG <sagg160800@gmail.com>
2024-04-26 10:51:19 -0400

    getMedicalHistoriesWithPatientsCIAndSpecialty

--- src/routes/medicalHistory.routes.js
+++ src/routes/medicalHistory.routes.js
@@ -1,10 +1,11 @@
 import { Router } from "express";
-import { createMedicalHistory, deleteMedicalHistory, getMedicalHistories, getMedicalHistoriesByPatient, getMedicalHistory, updateMedicalHistory } from "../controllers/medicalHistory.controllers.js";
+import { createMedicalHistory, deleteMedicalHistory, getMedicalHistories, getMedicalHistoriesByPatient, getMedicalHistoriesWithPatientCIAndSpecialty, getMedicalHistory, updateMedicalHistory } from "../controllers/medicalHistory.controllers.js";
 import {verifyToken, isReceptionistOrIsDoctor, isReceptionistOrIsDoctorOwnerMedicalHistory} from "../middlewares/index.js"
 
 const router = Router()
 
 router.get('/medical_histories', [verifyToken], getMedicalHistories)
+router.get('/medical_histories/patient/specialty', [verifyToken], getMedicalHistoriesWithPatientCIAndSpecialty)
 router.get('/medical_histories/patient/:patient_id', [verifyToken], getMedicalHistoriesByPatient)
 router.get('/medical_histories/:id', [verifyToken], getMedicalHistory)
 router.post('/medical_histories', [verifyToken, isReceptionistOrIsDoctor], createMedicalHistory)
