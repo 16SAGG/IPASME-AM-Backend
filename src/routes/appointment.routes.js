67e46821f7492e33b777a7b960c10d7d2620b2a6
16SAGG <sagg160800@gmail.com>
2024-04-28 19:08:42 -0400

    getAppointmentsNotIncludesInAMedicalHistory_route_added

--- src/routes/appointment.routes.js
+++ src/routes/appointment.routes.js
@@ -1,11 +1,12 @@
 import { Router } from "express";
-import { createAppointment, deleteAppointment, getAppointment, getAppointments, getAppointmentsByDoctor, getAppointmentsWithDoctorCIAndPatientCI, updateAppointment } from "../controllers/appointment.controllers.js";
+import { createAppointment, deleteAppointment, getAppointment, getAppointments, getAppointmentsByDoctor, getAppointmentsNotIncludesInAMedicalHistory, getAppointmentsWithDoctorCIAndPatientCI, updateAppointment } from "../controllers/appointment.controllers.js";
 import {isReceptionist, isReceptionistOrIsDoctor, isReceptionistOrIsDoctorOwnerAppointment, verifyToken} from "../middlewares/index.js"
 
 const router = Router()
 
 router.get('/appointments', [verifyToken, isReceptionist], getAppointments)
 router.get('/appointments/doctor/patient', [verifyToken, isReceptionist], getAppointmentsWithDoctorCIAndPatientCI)
+router.get('/appointments/whitout/medical_histories', [verifyToken], getAppointmentsNotIncludesInAMedicalHistory)
 router.get('/appointments/doctor/:doctor_id', [verifyToken, isReceptionistOrIsDoctor], getAppointmentsByDoctor)
 router.get('/appointments/:id', [verifyToken, isReceptionistOrIsDoctorOwnerAppointment], getAppointment)
 router.post('/appointments', [verifyToken, isReceptionist], createAppointment)
