fc05718951e9c0ed00ed6b4399d3db7d445d5894
16SAGG <sagg160800@gmail.com>
2024-04-28 16:25:05 -0400

    getDoctorsBySpecialtyAndTurn_route_added

--- src/routes/users.routes.js
+++ src/routes/users.routes.js
@@ -1,11 +1,12 @@
 import { Router } from "express";
-import { deleteUser, getUsers, getUser, updateUser, getDoctors } from "../controllers/users.controllers.js";
+import { deleteUser, getUsers, getUser, updateUser, getDoctors, getDoctorsBySpecialtyAndTurn } from "../controllers/users.controllers.js";
 import {isAdmin, verifyToken} from "../middlewares/index.js"
 
 const router = Router()
 
 router.get('/users', [verifyToken, isAdmin], getUsers)
 router.get('/users/doctors', verifyToken, getDoctors)
+router.get('/users/doctors/:specialty/:turn', verifyToken, getDoctorsBySpecialtyAndTurn)
 router.get('/users/:id', [verifyToken, isAdmin], getUser)
 router.patch('/users/:id', [verifyToken, isAdmin], updateUser )
 router.delete('/users/:id', [verifyToken, isAdmin], deleteUser)
