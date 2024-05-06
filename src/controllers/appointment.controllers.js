1bfc35f61e813945a8caa418b7f550e415dcaf86
16SAGG <sagg160800@gmail.com>
2024-05-05 20:05:20 -0400

    getAppointmentsByDoctor only show appointments from today to forward

--- src/controllers/appointment.controllers.js
+++ src/controllers/appointment.controllers.js
@@ -72,7 +72,7 @@ export const getAppointmentsByDoctor = async (req, res) =>{
         const currentDate = new Date();
         const year = currentDate.getFullYear();
         const month = currentDate.getMonth() + 1;
-        const day = fechaActual.getDate();
+        const day = currentDate.getDate();
         
         const dateFormat = `${year}-${month.toString().padStart(2, '0')}-${day.toString().padStart(2, '0')}`
 
